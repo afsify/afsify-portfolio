879f037bc7a8fb1665373c5e3aa576de3b24a197
AFSAL333 <afsalafs8008@gmail.com>
2024-04-13 18:59:48 +0530

    listdev removed

--- server/controller/user/devhub.controller.js
+++ server/controller/user/devhub.controller.js
@@ -1,26 +1,6 @@
 const developerModel = require("../../model/developer.model");
 const { body, validationResult } = require("express-validator");
 
-//! ============================================= List Developer =============================================
-
-const listDev = async (req, res, next) => {
-  try {
-    const devData = await developerModel
-      .findOne({ user: req.userId })
-      .populate("user", ["name", "email", "developer", "image"])
-      .sort({ createdAt: -1 })
-      .limit(1);
-    res.status(200).send({
-      message: "DevData Fetched Successfully",
-      success: true,
-      data: devData,
-    });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Error Occurred" });
-    next(error);
-  }
-};
-
 //! =========================================== Discover Developer ===========================================
 
 const discoverDev = async (req, res, next) => {
@@ -41,6 +21,5 @@ const discoverDev = async (req, res, next) => {
 };
 
 module.exports = {
-  listDev,
   discoverDev,
 };
