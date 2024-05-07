838143712989d70a82c25e40f4254b7cec6f1ff6
AFSAL333 <afsalafs8008@gmail.com>
2024-05-07 20:12:51 +0530

    dev remove removed

--- server/controller/admin/developer.controller.js
+++ server/controller/admin/developer.controller.js
@@ -1,32 +1,6 @@
 const developerModel = require("../../model/developer.model");
 const userModel = require("../../model/user.model");
 
-//! ============================================ Reject Request ============================================
-
-const rejectDev = async (req, res, next) => {
-  try {
-    const dev = await developerModel.findByIdAndUpdate(
-      req.params.devId,
-      { status: "rejected" },
-      { new: true }
-    );
-    if (!dev) {
-      return res.status(404).json({
-        message: "Developer Not Found",
-        success: false,
-      });
-    }
-    res.status(200).json({
-      message: "Request Accepted",
-      success: true,
-      dev,
-    });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Error Occurred" });
-    next(error);
-  }
-};
-
 //! ============================================ Remove Request ============================================
 
 const removeDev = async (req, res, next) => {
@@ -59,6 +33,5 @@ const removeDev = async (req, res, next) => {
 };
 
 module.exports = {
-  rejectDev,
   removeDev,
 };
